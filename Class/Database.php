ccc44688e40518ea8c95c0a81280aaecc9940024
Just call me zel <janzeldols@gmail.com>
2026-03-15 15:53:16 +0800

    initialize database connection

---
+++ Class/Database.php
@@ -0,0 +1,45 @@
+<?php
+
+class Database
+{
+    private $statement;
+    private $connection;
+
+    public function __construct($config, $username = 'root', $password = '')
+    {
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query, $param = [])
+    {
+        $this->statement = $this->connection->prepare($query);
+
+        $this->statement->execute($param);
+
+        return $this;
+    }
+
+    public function find()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function fetch_one()
+    {
+        return $this->statement->fetch();
+
+    }
+
+    public function lastInsertId()
+    {
+        return $this->connection->lastInsertId();
+    }
+
+    public function count()
+    {
+        return $this->statement->rowCount();
+    }
+}
